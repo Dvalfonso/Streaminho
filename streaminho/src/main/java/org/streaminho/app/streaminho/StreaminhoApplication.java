ae53c5f00ed10e8787ff224c453276006e077bdc
David <davidalfonsook@gmail.com>
2026-07-31 20:45:40 -0300

    Setup dev and prod profiles. Start postgresql service.

---
+++ streaminho/src/main/java/org/streaminho/app/streaminho/StreaminhoApplication.java
@@ -0,0 +1,13 @@
+package org.streaminho.app.streaminho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreaminhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreaminhoApplication.class, args);
+	}
+
+}
---
+++ streaminho/src/test/java/org/streaminho/app/streaminho/StreaminhoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.streaminho.app.streaminho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreaminhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
