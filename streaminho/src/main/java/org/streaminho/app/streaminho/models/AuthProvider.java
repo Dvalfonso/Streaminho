a78ff4d8874478a14d2e663b6935677da82547f0
David <davidalfonsook@gmail.com>
2026-07-31 21:18:15 -0300

    Add User model

---
+++ streaminho/src/main/java/org/streaminho/app/streaminho/models/AuthProvider.java
@@ -0,0 +1,7 @@
+package org.streaminho.app.streaminho.models;
+
+public enum AuthProvider {
+    LOCAL,
+    GOOGLE,
+    GITHUB
+}
---
+++ streaminho/src/main/java/org/streaminho/app/streaminho/models/Role.java
@@ -0,0 +1,7 @@
+package org.streaminho.app.streaminho.models;
+
+public enum Role {
+    ADMIN,
+    USER,
+    MODERATOR
+}
