d1442e239c827f95a10a181acef75730ac34a00c
davidalfonsook@gmail.com <david@DESKTOP-EAHC6JU.localdomain>
2026-08-12 21:40:13 +0000

    Set wsl

--- backend/streaminho/src/main/java/org/streaminho/app/streaminho/StreaminhoApplication.java
+++ backend/streaminho/src/main/java/org/streaminho/app/streaminho/StreaminhoApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StreaminhoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(StreaminhoApplication.class, args);
+        SpringApplication.run(StreaminhoApplication.class, args);
 	}
 
 }
